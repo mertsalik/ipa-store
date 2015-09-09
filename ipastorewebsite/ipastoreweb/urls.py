e33b459b50fcb64266a9b91ec270f05e9b697d25
Mert Salık <mertsalik.itu@gmail.com>
2015-09-10 02:41:23 +0300

    folder naming

--- ipastorewebsite/ipastoreweb/urls.py
+++ ipastorewebsite/ipastoreweb/urls.py
@@ -1,20 +1,9 @@
 __author__ = 'mertsalik'
 
 from django.conf.urls import patterns, url
-
 from . import views
 
-# urlpatterns = [
-#     # ex: /ipa-store/
-#     url(r'^$', views.index, name='index'),
-#     # ex: /ipa-store/5/
-#     url(r'^(?P<ipa_id>[0-9]+)/$', views.detail, name='detail'),
-#     # ex: /ipa-store/5/download/
-#     url(r'^(?P<ipa_id>[0-9]+)/download/$', views.download,
-#         name='download'),
-# ]
-
-urlpatterns = patterns('',
+urlpatterns = patterns('views',
                        url(r'^$', views.index, name='index'),
                        url(r'^(?P<ipa_id>[0-9]+)/$', views.detail,
                            name='detail'),
