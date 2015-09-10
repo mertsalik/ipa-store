7d23b12a602dfac7b3178eaefbe0d005211c1e36
Mert Salık <mertsalik.itu@gmail.com>
2015-09-10 03:45:39 +0300

    views

--- ipastorewebsite/ipastoreweb/models.py
+++ ipastorewebsite/ipastoreweb/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Ipa(models.Model):
-    id = models.IntegerField('Identifier',primary_key=True)
+    id = models.IntegerField('Identifier', primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     app_version = models.CharField(max_length=20)
--- ipastorewebsite/ipastoreweb/views.py
+++ ipastorewebsite/ipastoreweb/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from models import Ipa
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ipa-store index.")
+    latest_ipa_list = Ipa.objects.order_by('-pub_date')[:5]
+    context = {'latest_ipa_list': latest_ipa_list}
+    return render(request, 'ipastoreweb/index.html', context)
 
 
 def detail(request, ipa_id):
-    return HttpResponse("You're looking at ipa %s." % ipa_id)
+    try:
+        ipa = Ipa.objects.get(pk=ipa_id)
+    except Ipa.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'ipastoreweb/detail.html', {'ipa': ipa})
 
 
 def download(request, ipa_id):
